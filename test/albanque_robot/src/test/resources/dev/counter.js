a6495deee77eb6d7a96bcaa37e0c890c0bd4582a
Axel <axel.lenroue@gmail.com>
2019-06-21 11:05:10 +0200

    remove test

--- test/albanque_robot/src/test/resources/dev/counter.js
+++
@@ -1,4 +0,0 @@
-setTimeout(function(){ for(i=1; i< parent.document.querySelectorAll("body > div > section").length+1; i++){
-    console.log(parent.document.querySelectorAll("body > div > section:nth-child("+i+") > details > section > details > summary > div > span.tag")['0'].innerHTML + ": " + parent.document.querySelectorAll("body > div > section:nth-child("+i+") > details > section > details > ol > li.skipped").length);
-} }, 5000);
-
